3ef0e5227ad38b5dc109218e369395b359b51afc
Patricia <patricia.badji89@gmail.com>
2025-01-10 17:28:23 +0100

    starting

---
+++ src/interfaces/SetBrightness.java
@@ -0,0 +1,7 @@
+package interfaces;
+// Questa interfaccia è comune a Video e Immagine, contiene il metodo per aumentare e diminuire la luminosità (da 1 a 5)//
+
+public interface SetBrightness {
+    void brightnessUp();
+    void brightnessDown();
+}
